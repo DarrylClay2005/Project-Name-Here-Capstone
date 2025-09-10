d4c813389b1f562201e6763a8b3ede50079b514b
DarrylClay2005 <dclay2581@gmail.com>
2025-09-10 16:32:09 -0500

    fix: add navigation menu to Header component

--- cap-project/src/Header.jsx
+++ cap-project/src/Header.jsx
@@ -2,6 +2,13 @@ function Header(){
 return (
     <header>
         <h1>Dog Weight Scaler</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
     </header>
 )
 }
