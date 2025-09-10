119ca7d34cde2212c8c97f196dde6136e5182435
DarrylClay2005 <dclay2581@gmail.com>
2025-09-10 17:02:43 -0500

    Updated App.jsx to use Fish.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer"
 import Header from "./Header"
+import FishBreedFinder from "./Fish"
 
 function App() {
     return (
         <>
             <Header/>
+            <FishBreedFinder/>
             <Footer/>
         </>
     )
